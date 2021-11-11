40ecd12f4001d5213f8c772a096ffb35ac869d55
mscamargo <mscamargo.dev@gmail.com>
2021-11-11 09:23:19 -0300

    many improvements

--- install.sh
+++ install.sh
@@ -3,64 +3,60 @@
 # by Marcos Camargo <mscamargo.dev@gmail.com>
 # License: GNU GPLv3
 
-packages="https://raw.githubusercontent.com/mscamargo/uarbs/master/packages.list"
-debs="https://raw.githubusercontent.com/mscamargo/uarbs/master/debs.list"
-ppas="https://raw.githubusercontent.com/mscamargo/uarbs/master/ppas.list"
-sources="https://raw.githubusercontent.com/mscamargo/uarbs/master/sources.list"
+download () { clear; echo "Downloading $1..."; wget "$1" -O /tmp/$(basename "$1"); }
 
-download () {
-    wget "$1" -O /tmp/$(basename "$1") 
+download_installation_files () {
+    base_url = "https://raw.githubusercontent.com/mscamargo/uarbs/master"
+    download "$base_url/packages.list"
+    download "$base_url/debs.list"
+    download "$base_url/ppas.list"
+    download "$base_url/sources.list"
 }
 
-download https://raw.githubusercontent.com/mscamargo/uarbs/master/packages.list
-download https://raw.githubusercontent.com/mscamargo/uarbs/master/debs.list
-download https://raw.githubusercontent.com/mscamargo/uarbs/master/ppas.list
-download https://raw.githubusercontent.com/mscamargo/uarbs/master/sources.list
+add_source () { clear; echo "Adding source..."; echo "$1" | sudo tee /etc/apt/sources.list.d/$2; }
+
+add_sources () {
+    while IFS=, read -r source file; do
+        add_source "$source" "$file"
+    done < /tmp/sources.list
+}
+
+add_ppa () { clear; echo "Adding $1..."; sudo add-apt-repository -y "ppa:$1"; }
+
+add_ppas () {
+    while read ppa; do
+        add_ppa "$ppa"
+    done < /tmp/ppas.list
+}
 
 update () { clear; echo "Updating repositories..."; sudo apt update -y; }
 
-add_ppa () { clear; echo "Adding ppa $1..."; sudo add-apt-repository -y "$1"; }
-
-add_ppas () {
-    add_ppa ppa:regolith-linux/release
-}
-
-install () { clear; echo "Installing $1..."; sudo apt install -y "$1" ;}
+install () { clear; echo "Installing $1..."; sudo apt install -y "$1"; }
 
 install_packages () {
-    cp "$packages" /tmp/packages.list || wget "$packages" -O /tmp/packages.list
     while read package; do
         install "$packages"
     done < /tmp/packages.list
 }
 
-install_sddm () {
+install_deb () { clear; echo "Installing $(basename $1)"; download "$1"; install /tmp/$(basename $1); }
+
+install_debs () {
+    while read deb; do
+        install "$deb"
+    done < /tmp/debs.list
+}
+
+configure_sddm () {
     clear
-    install sddm
+    echo "Enabling SDDM..."
     sudo systemctl enable sddm
-    sudo mkdir -p /usr/share/sddm/themes
+    clear
+    echo "Setting Slice Theme..."
+    sudo mkdir -pv /usr/share/sddm/themes
     git clone https://github.com/RadRussianRus/sddm-slice.git /tmp/sddm-slice
-    sudo cp -r /tmp/sddm-slice /usr/share/sddm/themes/slice
+    sudo cp -rv /tmp/sddm-slice /usr/share/sddm/themes/slice
     sudo echo -e "[Theme]\nCurrent=slice" | sudo tee /etc/sddm.conf
-}
-
-install_brave () {
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    update
-    install brave-browser
-}
-
-install_discord () {
-    clear
-    wget https://dl.discordapp.net/apps/linux/0.0.16/discord-0.0.16.deb -O /tmp/discord.deb
-    install /tmp/discord.deb
-}
-
-install_zoom () {
-    clear
-    wget https://cdn.zoom.us/prod/5.8.3.145/zoom_amd64.deb -O /tmp/zoom.deb
-    install /tmp/zoom.deb
 }
 
 install_dots () {
@@ -76,14 +72,19 @@ install_dots () {
     rm README.md
 }
 
+download_installation_files
+
+# Add brave keys
+echo "Adding Brave Browser keys..."
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+add_sources
 add_ppas
 update
 install_packages
-install_sddm
+install_debs
+configure_sddm
 install_dots
-install_brave
-install_discord
-install_zoom
 
 clear
 
